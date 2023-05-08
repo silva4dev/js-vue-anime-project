aeed5617587f834422ef30beb10042265667ac1b
silva4dev <web4dev.lucas@outlook.com>
2023-05-08 00:21:54 -0300

    inicial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,143 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <h1>The<strong>Anime</strong></h1>
+      <form class="search-box" @submit.prevent="HandleSearch">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime..."
+          required
+          v-model="search_query"
+        />
+      </form>
+    </header>
+    <main>
+      <div class="cards" v-if="animelist && animelist.length > 0">
+        <Card v-for="anime in animelist" :anime="anime" :key="anime.mal_id" />
+      </div>
+      <div class="no-results" v-else>
+        <h3>Sorry, we have no results...</h3>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  setup() {
+    const search_query = ref("");
+    const animelist = ref([]);
+
+    const HandleSearch = async () => {
+      await fetch(
+        `https://api.jikan.moe/v4/anime?q=${search_query.value}`
+      ).then(async (res) => {
+        const response = await res.json();
+        animelist.value = response.data;
+      });
+      search_query.value = "";
+    };
+    return {
+      Card,
+      search_query,
+      animelist,
+      HandleSearch,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: "Fira Sans", sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+
+    strong {
+      color: #313131;
+    }
+
+    &:hover {
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+
+      background-color: #f3f3f3;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 8px;
+
+      color: #313131;
+      font-size: 20px;
+
+      transition: 0.4s;
+
+      &::placeholder {
+        color: #aaa;
+      }
+
+      &:focus,
+      &:valid {
+        color: #fff;
+        background-color: #313131;
+        box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.15);
+      }
+    }
+  }
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
+
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -8px;
+  }
 }
 </style>
